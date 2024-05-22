0b8877dcb32b37c18d8f35152b29e4ec1812d71d
MuhammadAbuBakarQureshi <qureshimuhammadabubakar@gmail.com>
2024-05-22 16:58:40 +0500

    Main Menu Updated

--- LMS/Main Menu/MainMenu.cpp
+++ LMS/Main Menu/MainMenu.cpp
@@ -16,11 +16,11 @@ void mainMenu(string books[], string authorname[], bool available[], int books_a
         cout << "\nMain Menu\n"
             << endl;
 
-        cout << "1. Library." << endl
-            << "2. Search." << endl
-            << "3. Add." << endl
-            << "4. Lend." << endl
-            << "5. exit." << endl;
+        cout << "1. List Books." << endl
+            << "2. Search Books." << endl
+            << "3. Add Books." << endl
+            << "4. Lend Book." << endl
+            << "5. exit Library." << endl;
 
         cout << "\nChoose one of em with sr.no : ";
 
@@ -63,63 +63,3 @@ void mainMenu(string books[], string authorname[], bool available[], int books_a
         }
     }
 }
-
-
-//void mainMenu()
-//{
-//
-//    int option = 0;
-//
-//    while (option != 5)
-//    {
-//
-//        cout << "\nMain Menu\n"
-//            << endl;
-//
-//        cout << "1. Library." << endl
-//            << "2. Search." << endl
-//            << "3. Add." << endl
-//            << "4. Lend." << endl
-//            << "5. exit." << endl;
-//
-//        cout << "\nChoose one of em with sr.no : ";
-//
-//        cin >> option;
-//
-//        // Function you made individually are going to be used in these cases
-//
-//        switch (option)
-//        {
-//        case 1:
-//            // library Function
-//            cout << "Library" << endl;
-//            break;
-//
-//        case 2:
-//            // Search Function
-//            cout << "Search" << endl;
-//            break;
-//
-//        case 3:
-//
-//            addingBooks();
-//            break;
-//
-//        case 4:
-//
-//            // Lend Function
-//            cout << "Lend" << endl;
-//            break;
-//
-//        case 5:
-//
-//            // exit
-//            cout << "Your exiting library" << endl;
-//            break;
-//        default:
-//
-//            cout << "\nPlease choose the given option with refrence of sr.no" << endl;
-//            break;
-//        }
-//    }
-//}
